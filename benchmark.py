7150f12b17697a2d4c1c61bc0d75275ddeaf9f88
juardilag <juardilag@unal.edu.co>
2026-01-08 07:28:08 -0500

    added GPU and CPU functions

---
+++ benchmark.py
@@ -0,0 +1,54 @@
+import jax
+import jax.numpy as jnp
+from functions import make_spin_system_functions, make_tavis_cummings_system_functions
+from initial_samplings import discrete_spin_sampling, boson_sampling
+import diffrax
+from tqdm import tqdm
+
+def run_benchmark(trajectory_counts, params):
+    
+    # Define Devices
+    try:
+        cpu = jax.devices("cpu")[0]
+        gpus = jax.devices("gpu")
+        gpu = gpus[0] if len(gpus) > 0 else None
+    except:
+        gpu = None
+        print("No GPU found. Benchmarking CPU only.")
+
+    # Prepare Solver components once
+    drift, diffusion = make_tavis_cummings_system_functions(
+        params['omega_c'], params['omega_s'], params['g'], 
+        params['kappa'], params['gamma_d'], params['gamma_u'], params['N']
+    )
+    t_eval = jnp.linspace(0, params['t_max'], int(1e3))
+
+    # Single Trajectory
+    @jax.jit
+    def solve_one(y0, key):
+        bm = diffrax.VirtualBrownianTree(t_eval[0], t_eval[-1]+0.1, tol=1e-3, shape=(2+4*params['N'],), key=key)
+        term = diffrax.MultiTerm(diffrax.ODETerm(drift), diffrax.ControlTerm(diffusion, bm))
+        sol = diffrax.diffeqsolve(term, diffrax.Heun(), t0=t_eval[0], t1=t_eval[-1], dt0=0.01, y0=y0, saveat=diffrax.SaveAt(ts=t_eval))
+        return sol.ys
+    
+    # Vectorized Batch of Trajectories
+    @jax.jit
+    def solve_batch(y0_batch, keys):
+        return jax.vmap(solve_one)(y0_batch, keys)
+    
+    # The generation of initial conditions can also be made in CPU or GPU
+    def get_data(n, device):
+        key = jax.random.PRNGKey(0)
+        k1, k2, k3, k4 = jax.random.split(key, 4)
+        
+        # Sampling
+        a = 0.5 * jax.random.normal(k1, (n, 1)) + 1j * 0.5 * jax.random.normal(k2, (n, 1))
+        sx = 2.0 * jax.random.bernoulli(k3, 0.5, (n, params['N'])) - 1.0
+        sy = 2.0 * jax.random.bernoulli(k4, 0.5, (n, params['N'])) - 1.0
+        sz = jnp.ones((n, params['N']))
+        y0 = jnp.concatenate([jnp.real(a), jnp.imag(a), jnp.stack([sx,sy,sz], axis=2).reshape(n, 3*params['N'])], axis=1)
+        
+        keys = jax.random.split(key, n)
+        
+        # Move to device
+        return jax.device_put(y0, device), jax.device_put(keys, device)
