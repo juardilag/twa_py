e848207182ce1ed1a6ded9a1f0a8e19c99c2ed8e
juardilag <juardilag@unal.edu.co>
2026-02-18 09:46:23 -0500

    optimized memory kernel

--- direct_integration.py
+++ direct_integration.py
@@ -1,33 +1,29 @@
 import jax.numpy as jnp
 from jax import vmap
 import jax
-from initial_samplings import discrete_spin_sampling_single, discrete_spin_sampling_factorized
+from initial_samplings import discrete_spin_sampling_factorized
 from tqdm.auto import tqdm
 
+jax.config.update("jax_enable_x64", True)
+
 def get_spectral_density(omega, eta, omega_c, s):
-    # J(omega) = eta * omega^s * omega_c^(1-s) * exp(-omega/omega_c)
     safe_omega = jnp.where(omega > 0, omega, 1.0) 
-    
-    # Using jnp.power once is more efficient
     J_val = eta * safe_omega * jnp.power(safe_omega / omega_c, s - 1) * jnp.exp(-safe_omega / omega_c)
-    
     return jnp.where(omega > 0, J_val, 0.0)
 
-def get_A_omega(omega, eta, omega_c, s):
-    """Spectral function A(omega) = 2 * J(omega) for positive omega."""
-    return 2.0 * get_spectral_density(omega, eta, omega_c, s)
 
+# Memory Kernel
 def compute_memory_kernel(tau_grid, eta, omega_c, s, g=1.0, num_omega=10_000):
-    omega_max = 50*omega_c
-    omega_grid = jnp.linspace(1e-1, omega_max, num_omega)
-    A_vals = get_A_omega(omega_grid, eta, omega_c, s)
+    omega_max = 50 * omega_c
     
-    def single_tau_kernel(t):
-        integrand = (A_vals / jnp.pi) * jnp.sin(omega_grid * t)
-        return g**2 * jnp.trapezoid(integrand, x=omega_grid)
+    omega_grid = jnp.linspace(1e-1, omega_max, num_omega)
+    A_vals = 2.0 * get_spectral_density(omega_grid, eta, omega_c, s)
+    sin_matrix = jnp.sin(jnp.outer(tau_grid, omega_grid))
 
-    return vmap(single_tau_kernel)(tau_grid)
-
+    integrand = (A_vals[None, :] / jnp.pi) * sin_matrix
+    integral = jnp.trapezoid(integrand, x=omega_grid, axis=1)
+    
+    return g**2 * integral
 
 def generate_noise(key, t_grid, eta, omega_c, s, kBT, g=1.0, num_omega=2000):
     omega_min = 1e-1  
